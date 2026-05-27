58d8c9e98835968250bddf8218ef1fc2277069b6
dailyshimul-hue <dailyshimul@gmail.com>
2026-05-27 21:19:49 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -38,7 +38,7 @@ def main():
     app = Application.builder().token(BOT_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     print("✅ Bot চালু হয়েছে...")
-    app.run_polling()
+    app.run_polling(drop_pending_updates=True)
 
 if __name__ == "__main__":
     main()
